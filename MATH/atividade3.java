c5aa97fbeb120d1d65ff591b094670b3c5cc7a5a
Giovana Zanoni <126972000+giovanazanonii@users.noreply.github.com>
2023-07-06 16:08:43 -0300

    Add files via upload
    Exercicios Math.

---
+++ MATH/atividade3.java
@@ -0,0 +1,13 @@
+public class atividade3 {
+    public static void main(String[] args) {
+
+        double Angulo = 60;
+
+        System.out.println("Calcular a tangente de um ângulo (VALOR) em radianos: " + Math.tan(Math.toRadians(Angulo)));
+        System.out.println("Calcular o arco seno do VALOR " + Math.asin(Math.toRadians(Angulo)));
+        System.out.println("Calcular o arco cosseno do VALOR " + Math.toRadians(Math.acos(Angulo)));
+        System.out.println("Calcular o arco tangente do VALOR " + Math.tan(Math.toRadians(Angulo)));
+        System.out.println("Calcular o logaritmo natural do VALOR " + Math.log(Angulo));
+    }
+}
+
